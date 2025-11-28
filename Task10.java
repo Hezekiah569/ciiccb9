2218460358d488da2f07385f015b1ae7a0f9dedb
Hezekiah Saif M. Palmes <131753431+Hezekiah569@users.noreply.github.com>
2025-11-28 01:59:58 +0000

    Created Task10.java

---
+++ Task10.java
@@ -0,0 +1,32 @@
+public class Task10 {
+    public static void main(String[] args) {
+        Student[] students = new Student[] {
+            new Student("Morgan", "Freeman"),
+            new Student("Brad", "Pitt"),
+            new Student("Kevin", "Spacey"),
+        };
+        for (Student s : students) {
+            s.printFullName();
+        }
+    }
+}
+
+class Student {
+    private String firstName;
+    private String lastName;
+    
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+// Your code goes here
+    public void printFullName() {
+        System.out.println(this.firstName + " " + this.lastName);
+    }
+}
+
